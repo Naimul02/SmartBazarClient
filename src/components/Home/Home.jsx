6cbb901873f7f2c1ff4e17de33e93090a17bb49b
Naimul02 <naimulislum39@gmail.com>
2024-11-12 12:49:20 -0800

    react project setup

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h3 className="text-xl font-bold">This is Home component</h3>
+        </div>
+    );
+};
+
+export default Home;
