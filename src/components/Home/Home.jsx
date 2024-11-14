8e2131e798cc8959f110bae7e703d3cfe3853603
Akahad1 <aksahad234567@gmail.com>
2024-11-14 08:41:58 +0600

    first commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 const Home = () => {
-    return (
-        <div>
-            <h3 className="text-xl font-bold">This is Home component</h3>
-        </div>
-    );
+  return (
+    <div>
+      <h3 className="text-xl font-bold">This is Home component</h3>
+    </div>
+  );
 };
 
 export default Home;
--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
-import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer/Footer';
+import Navbar from "../components/Navbar/Navbar";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer/Footer";
 
 const Main = () => {
-    return (
-        <div>
-            <Navbar></Navbar>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Main;
