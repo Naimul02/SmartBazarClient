fe9b5eac841b3e29f2d76ae4d19afbf4f016e6b1
shoriful-stack <sisazid182713@gmail.com>
2024-11-22 22:32:35 +0600

    hover effect updated on top section,hover effect added on link section, realtime year added on copyright section,font family added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,18 +9,20 @@ import {
   FaYoutube,
 } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
+import { IoMdCall, IoMdMail } from "react-icons/io";
 import { IoMailOpenSharp } from "react-icons/io5";
 import { TbCoinTakaFilled } from "react-icons/tb";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <footer className="bg-amber-50 pt-10 text-gray-700">
       {/* Top Icons Section */}
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 lg:max-w-screen-2xl lg:mx-auto px-1">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 lg:max-w-screen-2xl lg:mx-auto pl-2">
         <div className="flex items-center gap-3">
           <div className="flex items-center justify-center">
             <div className="rounded-full border-2 border-orange-400 w-[100px] h-[100px] flex items-center justify-center">
-              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:origin-center hover:-rotate-45 duration-300 hover:w-[75px] hover:h-[90px] hover:rounded-s-full">
+              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:rounded-tr-[140%] hover:rounded-bl-[140%]  hover:rounded-tl-[100%] hover:rounded-br-[100%]">
                 <FaTruckMoving className="text-white text-4xl" />
               </div>
             </div>
@@ -30,7 +32,7 @@ const Footer = () => {
         <div className="flex items-center gap-3">
           <div className="flex items-center justify-center">
             <div className="rounded-full border-2 border-orange-400 w-[100px] h-[100px] flex items-center justify-center">
-              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:origin-center hover:-rotate-45 duration-300 hover:w-[75px] hover:h-[90px] hover:rounded-s-full">
+              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:rounded-tr-[140%] hover:rounded-bl-[140%]  hover:rounded-tl-[100%] hover:rounded-br-[100%]">
                 <TbCoinTakaFilled className="text-white text-5xl" />
               </div>
             </div>
@@ -40,7 +42,7 @@ const Footer = () => {
         <div className="flex items-center gap-3">
           <div className="flex items-center justify-center">
             <div className="rounded-full border-2 border-orange-400 w-[100px] h-[100px] flex items-center justify-center">
-              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:origin-center hover:-rotate-45 duration-300 hover:w-[75px] hover:h-[90px] hover:rounded-s-full">
+              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:rounded-tr-[140%] hover:rounded-bl-[140%]  hover:rounded-tl-[100%] hover:rounded-br-[100%]">
                 <BiRefresh className="text-white text-5xl" />
               </div>
             </div>
@@ -50,7 +52,7 @@ const Footer = () => {
         <div className="flex items-center gap-3">
           <div className="flex items-center justify-center">
             <div className="rounded-full border-2 border-orange-400 w-[100px] h-[100px] flex items-center justify-center">
-              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:origin-center hover:-rotate-45 duration-300 hover:w-[75px] hover:h-[90px] hover:rounded-s-full">
+              <div className="w-[80px] h-[80px] rounded-full bg-orange-400 flex items-center justify-center transition hover:rounded-tr-[140%] hover:rounded-bl-[140%]  hover:rounded-tl-[100%] hover:rounded-br-[100%]">
                 <FaHeadphones className="text-white text-[32px]" />
               </div>
             </div>
@@ -65,9 +67,10 @@ const Footer = () => {
           <h2 className="text-4xl font-bold text-orange-400 mb-2">
             SmartBazar
           </h2>
-          <p className="text-base text-gray-700 font-medium">
-            Lorem ipsum isads simply dummy text of the printing and typesetting
-            industry.
+          <p className="text-gray-700 font-medium">
+            Smart Bazar, established in 2024, is one of the newest retail
+            supermarket chains in Bangladesh, currently operates in Dhaka &
+            Chattogram.
           </p>
         </div>
         {/* Address Section */}
@@ -75,8 +78,8 @@ const Footer = () => {
           <FaLocationDot className="text-4xl text-orange-400 " />
           <div>
             <h3 className="font-bold text-orange-400 text-lg mb-1">Address</h3>
-            <p>38 block street, area licard</p>
-            <p>Hamonia road, Sydney, Australia</p>
+            <p>House: 49, Road: 16 (27 Old),</p>
+            <p>Dhanmondi, Dhaka - 1209, Bangladesh</p>
           </div>
         </div>
         {/* Get In Touch Section */}
@@ -86,44 +89,58 @@ const Footer = () => {
             <h3 className="font-bold text-orange-400 text-lg mb-1">
               Get in touch
             </h3>
-            <p className="hover:text-orange-400 cursor-pointer">+8801844556677</p>
-            <p className="hover:text-orange-400 cursor-pointer">smartBazar60@gmail.com</p>
+            <a href="tel:+8801844556677" className="hover:text-orange-400 cursor-pointer flex gap-1 items-center">
+              <IoMdCall />
+              +8801844556677
+            </a>
+            <a href="mailto:smartBazar60@gmail.com" className="hover:text-orange-400 cursor-pointer flex items-center gap-2">
+              <IoMdMail />
+              smartBazar60@gmail.com
+            </a>
           </div>
         </div>
       </div>
 
       {/* Links Section */}
-      <div className="grid grid-cols-2 md:grid-cols-4 md:gap-8 gap-2 text-base text-gray-800 max-w-screen-2xl mx-auto my-9 px-2">
+      <div className="grid grid-cols-2 md:grid-cols-4 md:gap-8 gap-2 text-base text-gray-800 max-w-screen-2xl mx-auto my-9 pl-3 pr-0">
         {/* Top Categories Section */}
         <div>
           <h3 className="font-bold text-orange-400 mb-2 text-lg">
             Top categories
           </h3>
-          <ul>
-            <li>Fresh fruits</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-600">
+          <ul className="text-sm">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Fresh fruits
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-600 hover:text-orange-400 cursor-pointer">
               Organic wine
             </li>
-            <li>Organic juice</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-600">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Organic juice
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-600 hover:text-orange-400 cursor-pointer">
               Dairy & cheese
             </li>
-            <li>Fresh meat</li>
+            <li className="hover:text-orange-400 cursor-pointer">Fresh meat</li>
           </ul>
         </div>
         {/* Services Section */}
         <div>
           <h3 className="font-bold text-orange-400 mb-2 text-lg">Services</h3>
-          <ul>
-            <li>Fresh fruits</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-700">
+          <ul className="text-sm">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Fresh fruits
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-700 hover:text-orange-400 cursor-pointer">
               Organic wine
             </li>
-            <li>Organic juice</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-700">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Organic juice
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-700 hover:text-orange-400 cursor-pointer">
               Dairy & cheese
             </li>
-            <li>Fresh meat</li>
+            <li className="hover:text-orange-400 cursor-pointer">Fresh meat</li>
           </ul>
         </div>
 
@@ -132,32 +149,40 @@ const Footer = () => {
           <h3 className="font-bold text-orange-400 mb-2 text-lg">
             Privacy & terms
           </h3>
-          <ul>
-            <li>Payment policy</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-700">
+          <ul className="text-sm">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Payment policy
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-700 hover:text-orange-400 cursor-pointer">
               Privacy policy
             </li>
-            <li>Return policy</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-700">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Return policy
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-700 hover:text-orange-400 cursor-pointer">
               Shipping policy
             </li>
-            <li>Terms & conditions</li>
+            <li className="hover:text-orange-400 cursor-pointer">
+              Terms & conditions
+            </li>
           </ul>
         </div>
 
         {/* My Account Section */}
         <div>
           <h3 className="font-bold text-orange-400 mb-2 text-lg">My account</h3>
-          <ul>
-            <li>My account</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-700">
+          <ul className="text-sm">
+            <li className="hover:text-orange-400 cursor-pointer">My account</li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-700 hover:text-orange-400 cursor-pointer">
               My cart
             </li>
-            <li>Order history</li>
-            <li className="my-2 py-2 border-y border-dashed border-gray-700">
+            <li className="hover:text-orange-400 cursor-pointer">
+              Order history
+            </li>
+            <li className="my-2 py-2 border-y border-dashed border-gray-700 hover:text-orange-400 cursor-pointer">
               My wishlist
             </li>
-            <li>My address</li>
+            <li className="hover:text-orange-400 cursor-pointer">My address</li>
           </ul>
         </div>
       </div>
@@ -167,7 +192,7 @@ const Footer = () => {
         <div className="grid md:grid-cols-3 grid-cols-1 gap-2 py-4 mt-8 md:max-w-screen-2xl md:mx-auto items-center">
           <div>
             <p className="text-base text-white md:text-start text-center">
-              Copyright © 2024 by spacingtech
+            Copyright &copy; {currentYear} by <span className="font-bold">SmartBazar</span>
             </p>
           </div>
           <div className="grid grid-cols-5 gap-3 mx-auto">
@@ -200,21 +225,21 @@ const Footer = () => {
           <div className="grid md:grid-cols-6 grid-cols-4 px-2 bg-white">
             <div className="pl-1 pr-2 py-[9px]">
               <img
-                src="https://i.ibb.co.com/q7ZknLj/png-clipart-visa-logo-credit-card-visa-logo-payment-visa-blue-text-thumbnail1-removebg-preview.png"
+                src="https://res.cloudinary.com/dwtugdf8j/image/upload/v1732287034/png-clipart-visa-logo-credit-card-visa-logo-payment-visa-blue-text-thumbnail1-removebg-preview_d3wgmp.png"
                 alt=""
                 className="h-6"
               />
             </div>
             <div className="py-[2px] pl-4">
               <img
-                src="https://i.ibb.co.com/SBcy9nX/brandhistory-mc-vrt-120-2x1.png"
+                src="https://res.cloudinary.com/dwtugdf8j/image/upload/v1732287033/brandhistory_mc_vrt_120_2x1_ylbwwd.png"
                 alt=""
                 className="h-10"
               />
             </div>
             <div className="pr-4 py-[2px] col-span-2">
               <img
-                src="https://sslcommerz.com/wp-content/uploads/2021/11/logo.png"
+                src="https://res.cloudinary.com/dwtugdf8j/image/upload/v1732287033/logo1_joyskr.png"
                 alt=""
                 className="h-10"
               />
@@ -225,14 +250,14 @@ const Footer = () => {
                         </div> */}
             <div className="pl-2 md:pl-0 pr-2">
               <img
-                src="https://i.ibb.co.com/ZKxXfKD/Bkash-Customer-Care2.png"
+                src="https://res.cloudinary.com/dwtugdf8j/image/upload/v1732287034/Bkash-Customer-Care2_eayfal.png"
                 alt=""
                 className="h-10"
               />
             </div>
             <div className="pl-2">
               <img
-                src="https://i.ibb.co.com/6Z11whB/Nagad-Logo-wine1.png"
+                src="https://res.cloudinary.com/dwtugdf8j/image/upload/v1732287034/Nagad-Logo.wine1_mtf97k.png"
                 alt=""
                 className="h-10"
               />
